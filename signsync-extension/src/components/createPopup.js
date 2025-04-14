c73dac1ba83a8133c14ef6b33dd9aa211a48869f
Pedro Camargo Brito <pedro.ca.brito@hotmail.com>
2025-04-14 18:49:59 -0300

    Start to use fonts and now the website that opens the extension dont apply the css in the popup

--- signsync-extension/src/components/createPopup.js
+++ signsync-extension/src/components/createPopup.js
@@ -1,11 +1,41 @@
 import { createFooter } from './createFooter.js';
 import { createHeader } from './createHeader.js';
 import { createBody } from './createBody.js';
+import { injectFontAwesome } from './injectFontAwesome.js';
 
 export function createPopup() {
+  const wrapper = document.createElement("div");
+  wrapper.id = "SignSync-wrapper";
+
+  const shadow = wrapper.attachShadow({ mode: "open" });
+
+  // Injetar CSS externo e fontes
+  injectFontAwesome(shadow);
+
+  const fontURL = chrome.runtime.getURL('assets/fonts/Nunito/static/Nunito-Bold.ttf');
+  const cssURL = chrome.runtime.getURL("styles/style.css");
+
+  const style = document.createElement('style');
+  style.textContent = `
+  @font-face {
+    font-family: 'Nunito Bold';
+    src: url('${fontURL}') format('truetype');
+    font-weight: bold;
+    font-style: normal;
+  }
+
+  #SignSync {
+    font-family: 'Nunito Bold', sans-serif !important;
+  }
+`;
+
+
+  const link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = cssURL;
+
   const popup = document.createElement("div");
   popup.id = "SignSync";
-  popup.className = "SignSync";
 
   const header = createHeader(popup);
   const body = createBody('popup-body', '');
@@ -15,5 +45,9 @@ export function createPopup() {
   popup.appendChild(body);
   popup.appendChild(footer);
 
-  return popup;
+  shadow.appendChild(style);
+  shadow.appendChild(link);
+  shadow.appendChild(popup);
+
+  return wrapper;
 }
