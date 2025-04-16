acb477069fc28ded8c215404d43a763b76562c90
Pedro Camargo Brito <pedro.ca.brito@hotmail.com>
2025-04-16 19:30:20 -0300

    Change to only open when click in the extension

--- signsync-extension/src/content.js
+++ signsync-extension/src/content.js
@@ -2,7 +2,7 @@ import { injectFontAwesome } from './components/injectFontAwesome.js';
 import { createPopup } from './components/createPopup.js';
 import { enableDrag } from './components/drag.js';
 
-if (!document.getElementById("-wraSignSyncpper")) {
+if (!document.getElementById("SignSync-wrapper")) {
   injectFontAwesome();
   const popup = createPopup();
   document.body.appendChild(popup); 
